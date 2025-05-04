d2b472a54660b120f2710353df6f17bcfa2d737d
K hashimoto <keishi04hrikzira@gmail.com>
2025-05-04 18:29:04 +0900

    ✨  lambda powertools を用いたログ出力に変更

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,10 @@
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools import Logger
+
+logger = Logger()
 
 
+@logger.inject_lambda_context(log_event=True)
 def handler(event, context: LambdaContext):
-    print(f"{event=}")
-    print(f"{context=}")
+    logger.info(event)
+    logger.info(context)
