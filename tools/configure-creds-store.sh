2400b9b86218d0f24425c14d3ee415169f20c93b
K hashimoto <keishi04hrikzira@gmail.com>
2025-05-04 20:38:55 +0900

    😇  root ユーザーの場合の条件分岐が不要だったので削除

--- tools/configure-creds-store.sh
+++ tools/configure-creds-store.sh
@@ -2,10 +2,6 @@
 
 DOCKER_CONFIG_FILE="${HOME}/.docker/config.json"
 
-if [ "$(whoami)" == "root" ]; then
-    DOCKER_CONFIG_FILE="/root/.docker/config.json"
-fi
-
 
 THIS_DIR="$(dirname "${0}")"
 BASE_FILE_PATH="${THIS_DIR}/config.json"
