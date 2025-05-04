2d80d7954d11d41910bc7a29e957796c7dd90767
K hashimoto <keishi04hrikzira@gmail.com>
2025-05-04 20:42:56 +0900

    😇  config.json が存在しない場合には単にファイルを作成して終了するように修正

--- tools/configure-creds-store.sh
+++ tools/configure-creds-store.sh
@@ -6,5 +6,9 @@ DOCKER_CONFIG_FILE="${HOME}/.docker/config.json"
 THIS_DIR="$(dirname "${0}")"
 BASE_FILE_PATH="${THIS_DIR}/config.json"
 
+if [ ! -f "${DOCKER_CONFIG_FILE}" ]; then
+  envsubst < "${BASE_FILE_PATH}" | tee "${DOCKER_CONFIG_FILE}"
+fi
+
 cp -f "${DOCKER_CONFIG_FILE}"{,.bak}
 envsubst < "${BASE_FILE_PATH}" | tee "${DOCKER_CONFIG_FILE}"
