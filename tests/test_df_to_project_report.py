a191fe2248f073718c39cb5ebb92e9d086b1943d
K hashimoto <keishi04hrikzira@gmail.com>
2025-05-05 16:18:28 +0900

    💚 単体テスト追加

---
+++ tests/test_df_to_project_report.py
@@ -0,0 +1,116 @@
+import pytest
+
+import sys
+
+sys.path.append("..")
+from lambda_function import df_to_project_report, WeeklyProjectReport, ManHour
+import polars as pl
+
+from typing import TypeAlias
+
+Df: TypeAlias = pl.DataFrame
+
+
+@pytest.mark.parametrize(
+    argnames=["df", "expected"],
+    argvalues=[
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001"],
+                    "案件名": ["開発案件A"],
+                    "名前": ["太郎"],
+                    "工数": [4.0],
+                }
+            ),
+            [
+                WeeklyProjectReport(
+                    project_code="A001",
+                    name="開発案件A",
+                    man_hours=[ManHour(name="太郎", hour=4.0)],
+                )
+            ],
+            id="Only one row.",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001", "A001"],
+                    "案件名": ["開発案件A", "開発案件A"],
+                    "名前": ["太郎", "次郎"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            [
+                WeeklyProjectReport(
+                    project_code="A001",
+                    name="開発案件A",
+                    man_hours=[
+                        ManHour(name="太郎", hour=4.0),
+                        ManHour(name="次郎", hour=2.0),
+                    ],
+                )
+            ],
+            id="One project, two members.",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001", "B001"],
+                    "案件名": ["開発案件A", "開発案件B"],
+                    "名前": ["太郎", "次郎"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            [
+                WeeklyProjectReport(
+                    project_code="A001",
+                    name="開発案件A",
+                    man_hours=[
+                        ManHour(name="太郎", hour=4.0),
+                    ],
+                ),
+                WeeklyProjectReport(
+                    project_code="B001",
+                    name="開発案件B",
+                    man_hours=[
+                        ManHour(name="次郎", hour=2.0),
+                    ],
+                ),
+            ],
+            id="Two projects, one member for each.",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001", "B001", "A001", "B001"],
+                    "案件名": ["開発案件A", "開発案件B", "開発案件A", "開発案件B"],
+                    "名前": ["太郎", "次郎", "次郎", "太郎"],
+                    "工数": [4.0, 2.0, 1.3, 4.5],
+                }
+            ),
+            [
+                WeeklyProjectReport(
+                    project_code="A001",
+                    name="開発案件A",
+                    man_hours=[
+                        ManHour(name="太郎", hour=4.0),
+                        ManHour(name="次郎", hour=1.3),
+                    ],
+                ),
+                WeeklyProjectReport(
+                    project_code="B001",
+                    name="開発案件B",
+                    man_hours=[
+                        ManHour(name="次郎", hour=2.0),
+                        ManHour(name="太郎", hour=4.5),
+                    ],
+                ),
+            ],
+            id="Two projects, two members for each, random order.",
+        ),
+    ],
+)
+def test(df: Df, expected: WeeklyProjectReport):
+    actual = df_to_project_report(df)
+    assert actual == expected
---
+++ tests/test_process_df.py
@@ -0,0 +1,135 @@
+import pytest
+import polars as pl
+from typing import TypeAlias
+
+import sys
+
+sys.path.append("..")
+from lambda_function import process_df
+
+Df: TypeAlias = pl.DataFrame
+
+base = {
+    "Pコード": ["A001", "A001", "B002", "B002"],
+    "名前": ["太郎", "太郎", "太郎", "次郎"],
+    "日付": ["4/1", "4/2", "4/1", "4/2"],
+    "工数": [4, 2, 4, 1.0],
+}
+
+
+@pytest.mark.parametrize(
+    argnames=["df", "expected"],
+    argvalues=[
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001"],
+                    "名前": ["太郎"],
+                    "日付": ["4/1"],
+                    "工数": [4.0],
+                }
+            ),
+            Df(
+                {
+                    "Pコード": ["A001"],
+                    "名前": ["太郎"],
+                    "工数": [4.0],
+                }
+            ),
+            id="Only one row",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001", "A001"],
+                    "名前": ["太郎", "太郎"],
+                    "日付": ["4/1", "4/2"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            Df(
+                {
+                    "Pコード": ["A001"],
+                    "名前": ["太郎"],
+                    "工数": [6.0],
+                }
+            ),
+            id="One project, one member, two date.",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001", "A001"],
+                    "名前": ["太郎", "次郎"],
+                    "日付": ["4/1", "4/2"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            Df(
+                {
+                    "Pコード": ["A001", "A001"],
+                    "名前": ["太郎", "次郎"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            id="One project, two members, one date for each.",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": ["A001", "B002"],
+                    "名前": ["太郎", "太郎"],
+                    "日付": ["4/1", "4/1"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            Df(
+                {
+                    "Pコード": ["A001", "B002"],
+                    "名前": ["太郎", "太郎"],
+                    "工数": [4.0, 2.0],
+                }
+            ),
+            id="Two projects, one member, one date for each.",
+        ),
+        pytest.param(
+            Df(
+                {
+                    "Pコード": [
+                        "A001",
+                        "B002",
+                        "A001",
+                        "B002",
+                        "A001",
+                        "B002",
+                        "A001",
+                        "B002",
+                    ],
+                    "名前": [
+                        "太郎",
+                        "太郎",
+                        "太郎",
+                        "太郎",
+                        "次郎",
+                        "次郎",
+                        "次郎",
+                        "次郎",
+                    ],
+                    "日付": ["4/1", "4/2", "4/2", "4/3", "4/1", "4/2", "4/2", "4/3"],
+                    "工数": [4.0, 2.0, 1.5, 3.0, 4.0, 2.0, 1.5, 3.0],
+                }
+            ),
+            Df(
+                {
+                    "Pコード": ["A001", "B002", "A001", "B002"],
+                    "名前": ["太郎", "太郎", "次郎", "次郎"],
+                    "工数": [5.5, 5.0, 5.5, 5.0],
+                }
+            ),
+            id="Two projects, two members, two dates for each.",
+        ),
+    ],
+)
+def test(df: Df, expected: Df):
+    actual = process_df(df)
+    assert actual.to_dicts() == expected.to_dicts()
